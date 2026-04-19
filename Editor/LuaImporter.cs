3ed6743d481d553e15f1ee9c08fb215285c9bb26
EncodeTheCode <22122456+EncodeTheCode@users.noreply.github.com>
2026-04-19 19:07:35 +0200

    Update LuaImporter.cs
    I added a commit that adds the option to include Python compatability for the future, currently the code still accepts Lua code as you would normally expect, but in the future perhaps there will be Python support which could be beneficial for the SplashEdit project. Some game developers are requesting this to be added eventually, so that it can open more doors for game developers that are more familiar with the Python language and it'd make more people want to join in and develope for this project and propose suggestions on things will keeping things fully compatible with previous code.

--- Editor/LuaImporter.cs
+++ Editor/LuaImporter.cs
@@ -1,25 +1,67 @@
 using UnityEngine;
-using System.IO;
 using UnityEditor;
 using UnityEditor.AssetImporters;
+using System.IO;
+
 using SplashEdit.RuntimeCode;
 
 namespace SplashEdit.EditorCode
 {
+#if LUA_SUPPORT && PYTHON_SUPPORT
+    [ScriptedImporter(2, new[] { "lua", "py" })]
+    public class ScriptImporter : ScriptedImporter
+#elif LUA_SUPPORT
     [ScriptedImporter(2, "lua")]
-    class LuaImporter : ScriptedImporter
+    public class ScriptImporter : ScriptedImporter
+#elif PYTHON_SUPPORT
+    [ScriptedImporter(2, "py")]
+    public class ScriptImporter : ScriptedImporter
+#else
+    // Nothing enabled, no importer compiled, zero risk. In this specific else case do not declare ScriptImporter at all.
+    // The if elif statements determine whether you allow either Lua or Python support or just one of them. Python support might need to be added incase Python were to be allowed. I hope that Python support will be added eventually, it'd be greatly beneficial to this project.
+#endif
+#if LUA_SUPPORT || PYTHON_SUPPORT
     {
         public override void OnImportAsset(AssetImportContext ctx)
         {
-            var asset = ScriptableObject.CreateInstance<LuaFile>();
-            var luaCode = File.ReadAllText(ctx.assetPath);
-            asset.Init(luaCode);
-            asset.name = Path.GetFileName(ctx.assetPath);
-            var text = new TextAsset(asset.LuaScript);
+            string ext = Path.GetExtension(ctx.assetPath).ToLowerInvariant();
+            string code = File.ReadAllText(ctx.assetPath);
+            string fileName = Path.GetFileName(ctx.assetPath);
 
+            // Always safe fallback
+            var text = new TextAsset(code);
+            text.name = fileName;
             ctx.AddObjectToAsset("Text", text);
-            ctx.AddObjectToAsset("Script", asset);
-            ctx.SetMainObject(asset);  // LuaFile is the main object, not TextAsset
+
+#if LUA_SUPPORT
+            if (ext == ".lua")
+            {
+                var asset = ScriptableObject.CreateInstance<LuaFile>();
+                asset.Init(code);
+                asset.name = fileName;
+
+                ctx.AddObjectToAsset("Script", asset);
+                ctx.SetMainObject(asset);
+                return;
+            }
+#endif
+
+#if PYTHON_SUPPORT
+            if (ext == ".py")
+            {
+                var asset = ScriptableObject.CreateInstance<PythonFile>();
+                asset.Init(code);
+                asset.name = fileName;
+
+                ctx.AddObjectToAsset("Script", asset);
+                ctx.SetMainObject(asset);
+                return;
+            }
+#endif
+
+            // Absolute safety fallback (should never hit, but prevents import failure)
+            ctx.SetMainObject(text);
         }
     }
+#endif
 }
